003a848b7f1ca4a6c0b5ecaddb578fa997a25c4e
johannpa <johann_paque@hotmail.fr>
2022-07-21 20:03:56 +0200

    play with spring boot

---
+++ src/main/java/com/example/helloWorld/demo/helloWorld/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.helloWorld.demo.helloWorld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+@RequestMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
